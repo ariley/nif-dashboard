645c9384b190d3f76b1fd4fb0e0420fb2ec4ca40
Agnes Riley <riley45@llnl.gov>
2024-08-21 19:10:03 -0700

    chagintgoutdir

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,13 @@ import Icons from "unplugin-icons/vite";
 import tailwind from "@astrojs/tailwind";
 import IconsResolver from "unplugin-icons/resolver";
 import Components from "unplugin-vue-components/vite";
-// loader helpers
 import { FileSystemIconLoader } from "unplugin-icons/loaders";
 import prefetch from "@astrojs/prefetch";
-
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  server: {
-    proxy: {
-      './src/upload': 'http://localhost:3000',
-    },
-  },
   site: 'https://nif-dashboard-riley45-c15310d27d07474987c11102447766eb05df9900c.sd-pages.llnl.gov/',
-  outDir: 'public',
-  publicDir: './', // Set publicDir to root
+  outDir: 'dist',  // Output to "dist" folder
   vite: {
     resolve: {
       alias: {
@@ -36,10 +27,6 @@ export default defineConfig({
       }),
       Icons({
         customCollections: {
-          // key as the collection name
-          // a helper to load icons from the file system
-          // files under `./assets/icons` with `.svg` extension will be loaded as it's file name
-          // you can also provide a transform callback to change each icon (optional)
           "my-icons": FileSystemIconLoader("src/assets/icons", (svg) =>
             svg.replace(/^<svg /, '<svg fill="currentColor" ')
           ),
@@ -48,18 +35,10 @@ export default defineConfig({
       }),
     ],
   },
-  // Enable Vue to support Vue components.
   integrations: [
-    vue({
-    //   appEntrypoint: "/src/pages/_app",
-    }),
+    vue(),
     tailwind(),
     prefetch(),
-    sitemap({
-      // filter: (page) =>
-        // page !== "https://www.yoursite.com/checkout/" &&
-        // page !== "https://www.yoursite.com/checkout-cart/" &&
-    }),
+    sitemap(),
   ],
-  
 });
